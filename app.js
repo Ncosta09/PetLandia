8fffbd2bb4a36499082d975e76ecf60f7b1d38dc
Ncosta09 <ncosta981@gmail.com>
2023-06-06 17:17:57 -0300

    creacion de archivos y servidor con express corriendo

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const publicpath = path.resolve(__dirname, './public');
+app.use( express.static(publicpath) );
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/index.html'));
+});
+
+app.listen(3200, () => {
+    console.log("Servidor corriendo en el puerto 3200");
+});
