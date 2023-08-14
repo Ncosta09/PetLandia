fbdf07998b532d471f3352f39547c5d1488e48d7
Ncosta09 <ncosta981@gmail.com>
2023-08-14 16:43:47 -0300

    validaciones del registro en carpeta de middlewares

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,14 +1,12 @@
 const userController = require('./../controllers/userController');
-const User = require('../model/Usuario');
 const guestMiddleware = require('../middlewares/guestMiddleware');
 const authMiddleware = require('../middlewares/authMiddleware');
+const validations = require('../middlewares/validacionesRegistroMiddleware');
 
 
 const path = require('path');
 const express = require('express');
 const router = express.Router();
-const { body } = require('express-validator');
-
 
 const multer = require('multer');
 
@@ -23,48 +21,6 @@ const configuracionFotoPerfil = multer.diskStorage({
 });
 
 const uploadFile = multer({ storage: configuracionFotoPerfil });
-
-const validations = [
-    body('nombre').notEmpty().withMessage('Escribir un Nombre valido'),
-    body('apellido').notEmpty().withMessage('Escribir un Apellido valido'),
-    body('email').notEmpty().withMessage('Escribir un email valido').bail().isEmail()
-    .withMessage('Debes escribir un formato de correo valido').bail().custom((value, { req }) => {
-        let mailExistente = User.findByField('email', req.body.email);
-
-        if(mailExistente){
-            throw new Error('Este correo ya se encuentra registrado');
-        }
-        
-        return true;
-    }),
-    body('contrasena').notEmpty().withMessage('Se require contraseña').bail()
-    .isLength({ min: 5 }).withMessage('La contraseña debe contener al menos 5 caracteres'), 
-    body('confirmarContrasena').custom((value, { req }) => {
-      if (value !== req.body.contrasena) {
-        throw new Error('Las contraseñas no coinciden');
-      }
-
-      return true;
-
-    }),
-    body('fotoPerfil').custom((value, {req}) => {
-        let file = req.file;
-        let extencionesPass = ['.jpg', '.png', '.jpeg'];
-
-        if(!file){
-            throw new Error('Tenes que subir una imagen');
-        }
-        else{
-
-            let fileExtencion = path.extname(file.originalname);
-            if(!extencionesPass.includes(fileExtencion)){
-                throw new Error('La extencion del archivo no es la permitida');
-            }    
-        }
-
-        return true;
-    })
-]
 
 //Registro
 router.get('/register', guestMiddleware, userController.registrarse); //Forrmulario
