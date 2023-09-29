56c0fccbb6540dadfe97c1bb579542d9039f22ba
Ncosta09 <ncosta981@gmail.com>
2023-09-29 10:03:11 -0300

    cambio de font, agregado de boton para ver todos los productos y cambios en el filtro de animales con agregado de su funcionamiento

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -8,6 +8,40 @@ const mainController = {
         .then((resultado)  => { 
             res.render('index', {productos: resultado, usuario: req.session.usuarioLogeado});
 		});
+    },
+
+    todosProductos: (req, res) => {
+        db.Producto.findAll()
+        .then((resultado)  => { 
+            res.render('todosProductos', {productos: resultado, usuario: req.session.usuarioLogeado});
+		});
+    },
+
+    productosXcategoria: async (req, res) => {
+		let nombreAnimal = req.params.idCategoria;
+
+        if(nombreAnimal == 'Perro'){
+            idAnimal = 1;
+        }else if(nombreAnimal == 'Gato'){
+            idAnimal = 2;
+        }else if(nombreAnimal == 'Roedor'){
+            idAnimal = 3;
+        }else if(nombreAnimal == 'Ave'){
+            idAnimal = 4;
+        }else if(nombreAnimal == 'Pez'){
+            idAnimal = 5;
+        }
+
+        let productos = await db.Producto.findAll({ 
+            where: { 
+                Animal_FK: idAnimal
+            }
+        });
+		let categorias = await db.Categoria.findAll();
+		let animales = await db.Animal.findAll();
+		let marcas = await db.Marca.findAll();
+
+		res.render('productoCategoria', {productoXcategoria: productos, categorias, animales, marcas, usuario: req.session.usuarioLogeado});
     }
 }
 
--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -3,6 +3,13 @@ const mainController = require('./../controllers/mainController')
 const express = require('express');
 const router = express.Router();
 
+//index
 router.get('/', mainController.index);
+
+//todos los productos
+router.get('/productos', mainController.todosProductos);
+
+//todos los productos de una categoria
+router.get('/productos/categoria/:idCategoria', mainController.productosXcategoria);
 
 module.exports = router;
