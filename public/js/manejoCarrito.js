bd7624ee9fdfca55aa5a2bf1bf38f3b3236932d8
Ncosta09 <ncosta981@gmail.com>
2023-10-09 23:54:14 -0300

    agregado de funcion a boton comprar ahora

--- public/js/manejoCarrito.js
+++ public/js/manejoCarrito.js
@@ -1,5 +1,6 @@
 let usuarioIdentificacion = document.getElementById('userId').value;
 const agregarAlCarrito = document.querySelectorAll('#btnAgregar');
+const comprarAhora = document.querySelectorAll('#btnComprar');
 
 let carrito = [];
 
@@ -11,52 +12,63 @@ function guardarCarritoEnLocalStorage() {
     localStorage.setItem(`carrito_${usuarioIdentificacion}`, JSON.stringify(carrito));
 }
 
-agregarAlCarrito.forEach(button => {
-    button.addEventListener('click', function(event) {
-        let productId = document.querySelector('#id-item').value;
-        let productName = document.querySelector('#id-nombre').textContent;
-        let productPrice = parseFloat(document.querySelector('#id-precio').textContent.replace('$', '').trim());
-        let productDiscount = document.querySelector('#id-descuento');
-        if(productDiscount == 0 || productDiscount == null){
-            productDiscount = 0;
-        }else{
-            productDiscount = parseFloat(document.querySelector('#id-descuento').textContent.replace('% OFF', '').trim());
-        }
-        let productQuantity = parseInt(document.querySelector('#id-cantidad').value);
-        let productShip = document.querySelector('#id-envio');
-        if(productShip == 0 || productShip == null){
-            productShip = 0;
-        }else{
-            productShip = parseFloat(document.querySelector('#id-envio').textContent.replace('$', '').trim());
-        }
-        let productImage = document.querySelector('#id-imagen').src;
+function obtenerProducto(){
+    let productId = document.querySelector('#id-item').value;
+    let productName = document.querySelector('#id-nombre').textContent;
+    let productPrice = parseFloat(document.querySelector('#id-precio').textContent.replace('$', '').trim());
+    let productDiscount = document.querySelector('#id-descuento');
+    if(productDiscount == 0 || productDiscount == null){
+        productDiscount = 0;
+    }else{
+        productDiscount = parseFloat(document.querySelector('#id-descuento').textContent.replace('% OFF', '').trim());
+    }
+    let productQuantity = parseInt(document.querySelector('#id-cantidad').value);
+    let productShip = document.querySelector('#id-envio');
+    if(productShip == 0 || productShip == null){
+        productShip = 0;
+    }else{
+        productShip = parseFloat(document.querySelector('#id-envio').textContent.replace('$', '').trim());
+    }
+    let productImage = document.querySelector('#id-imagen').src;
         
-        const producto = {
-            id: productId,
-            nombre: productName,
-            precio: productDiscount ? (productPrice - (productPrice * productDiscount / 100)) : productPrice,
-            descuento: productDiscount ? productDiscount : 0,
-            cantidad: productQuantity,
-            envio: productShip,
-            imagen: productImage
-        };
+    const producto = {
+        id: productId,
+        nombre: productName,
+        precio: productDiscount ? (productPrice - (productPrice * productDiscount / 100)) : productPrice,
+        descuento: productDiscount ? productDiscount : 0,
+        cantidad: productQuantity,
+        envio: productShip,
+        imagen: productImage
+    };
 
-        console.log(producto)
+    console.log(producto)
 
-        let productoExistente;
+    let productoExistente;
 
-        for (let i = 0; i < carrito.length; i++) {
-            if (carrito[i].nombre == producto.nombre) {
-                productoExistente = carrito[i];
-            }
+    for (let i = 0; i < carrito.length; i++) {
+        if (carrito[i].nombre == producto.nombre) {
+            productoExistente = carrito[i];
         }
+    }
 
-        if (productoExistente) {
-            productoExistente.cantidad += productQuantity;
-        } else {
-            carrito.push(producto);
-        }
+    if (productoExistente) {
+        productoExistente.cantidad += productQuantity;
+    } else {
+        carrito.push(producto);
+    }
 
-        guardarCarritoEnLocalStorage();
+    guardarCarritoEnLocalStorage();
+}
+
+//AGREGAR AL CARRITO
+agregarAlCarrito.forEach(button => {
+    button.addEventListener('click', obtenerProducto);
+});
+
+//COMPRAR AHORA
+comprarAhora.forEach(button => {
+    button.addEventListener('click', function(event) {
+        obtenerProducto();
+        window.location.href = '/producto/carrito';
     });
 });
